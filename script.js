2a807e20660744a4baacb6cb477abc2986de9996
mIksrigz <m.iksrigz@gmail.com>
2024-02-22 12:57:16 +0100

    54%

---
+++ script.js
@@ -0,0 +1,32 @@
+const calorieCounter = document.getElementById('calorie-counter');
+const budgetNumberInput = document.getElementById('budget');
+const entryDropdown = document.getElementById('entry-dropdown');
+const addEntryButton = document.getElementById('add-entry');
+const clearButton = document.getElementById('clear');
+const output = document.getElementById('output');
+
+let isError = false;
+
+function cleanInputString(str) {
+    const regex = /[+-\s]/g;
+    return str.reaplace(regex, '');
+}
+
+function isInvalidInput(str) {
+    const regex = /\d+e\d+/i;
+    return str.match(regex);
+}
+
+function addEntry() {
+    const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+    const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
+    const HTMLString = `
+    <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
+    <input id="${entryDropdown.value}-${entryNumber}-name" type="text" placeholder="Name">
+    <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+    <input id="${entryDropdown.value}-${entryNumber}-calories" type="number" min="0" placeholder="Calories">
+    `;
+    targetInputContainer.innerHTML += HTMLString;
+}
+
+addEntryButton.addEventListener('click', addEntry);
